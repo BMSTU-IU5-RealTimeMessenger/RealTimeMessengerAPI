9d4efe0ad7d5209e7ec75ad836c66976191c465f
Dmitriy Permyakov <dimapermyakov55@gmail.com>
2024-03-08 16:43:05 +0300

    iOS-1: add pretty print package

--- Package.swift
+++ Package.swift
@@ -8,12 +8,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.89.0"),
+        .package(url: "https://github.com/YusukeHosonuma/SwiftPrettyPrint.git", .upToNextMajor(from: "1.4.0")),
     ],
     targets: [
         .executableTarget(
             name: "App",
             dependencies: [
                 .product(name: "Vapor", package: "vapor"),
+                .product(name: "SwiftPrettyPrint", package: "SwiftPrettyPrint")
             ]
         )
     ]
